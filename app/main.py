db9b1063145c1d01f402a398556ea9fef9cbb82c
“Divyanshu9794” <sdivyanshu352.com>
2024-05-01 15:21:46 +0530

    .

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import streamlit as st
-
+ 
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 model_path = f"{working_dir}/trained_model/plant_diseaseprediction.h5"
